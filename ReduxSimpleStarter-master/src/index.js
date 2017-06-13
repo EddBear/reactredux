10549d92c5423c45f37565af09da3e5cfde654ec
Edd <eisv.mora@gmail.com>
2017-06-12 19:13:28 -0500

    Latest

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
+const API_KEY = 'AIzaSyAEhJ_gLarQ7-JCBxIquVV05vG7B_Aaoyw';
 // Create a new component and this component should product some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+      <div>
+      <SearchBar/>
+      </div>
+  );
 }
 
 // Take this comoponent's generated HTML and put it on the page (DOM)
